868c8cdb5a9bf7f933294486d5c89d6233169388
Gabriel Cruz <gabs.oficial98@gmail.com>
2018-08-28 22:00:14 -0300

    Add infinite menu

--- src/main.py
+++ src/main.py
@@ -24,24 +24,24 @@ def print_menu():
     """)
 
 if __name__ == "__main__":
-    print_menu()
-    option = get_option() # not implemented correctly, lacks input validation
-
-    if options[option] == 'START':
-        print('Starting')
-        pass
-    elif options[option] == 'ADD':
-        print('Adding')
-        pass
-    elif options[option] == 'REMOVE':
-        print('Removing')
-        pass
-    elif options[option] == 'EDIT':
-        print('Editing')
-        pass
-    else: # EXIT
-        print('Exiting')
-        sys.exit()
+    while True:
+        print_menu()
+        option = get_option() # not implemented correctly, lacks input validation
+        if options[option] == 'START':
+            print('Starting')
+            pass
+        elif options[option] == 'ADD':
+            print('Adding')
+            pass
+        elif options[option] == 'REMOVE':
+            print('Removing')
+            pass
+        elif options[option] == 'EDIT':
+            print('Editing')
+            pass
+        else: # EXIT
+            print('Exiting')
+            sys.exit()
 
 
 
